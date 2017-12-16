ec2a0e2c03a4918d44579d6d4e98309d9b288446
Corey Martin <coreywmartin@gmail.com>
2017-12-16 13:54:49 -0500

    closes #10

---
+++ Assets/Scripts/ToggleAudio.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleAudio : MonoBehaviour {
+
+	public void ToggleSound() {
+		AudioListener.pause = !AudioListener.pause;
+	}
+}
